fee4cdfc49281e196e98710f87f95a098769cde2
Melissa <melisapaksoy@hotmail.com>
2025-03-04 10:47:23 -0500

    updating sys operations

--- Week09/sys_operations.py
+++ Week09/sys_operations.py
@@ -1,4 +1,6 @@
 import platform
+import socket
+import os
 
 print("Current machine type")
 print(platform.machine()) # Apple M1, M2, M3, M4, Raspberry Pi : arm64; for intel/AMD: x86
@@ -6,4 +8,25 @@ print ("-------------------------------\n")
 
 print("Current Processor Type:")
 print(platform.architecture())
+print ("-------------------------------\n")
+
+print("Set timeout of socket in seconds")
+print(socket.setdefaulttimeout(50))
+print ("-------------------------------\n")
+
+
+print("Get timeout of socket in seconds")
+print(socket.getdefaulttimeout())
+print ("-------------------------------\n")
+
+print("Operating System type")
+print(os.name)
+print ("-------------------------------\n")
+
+print("Operating System name")
+print(platform.system())
+print ("-------------------------------\n")
+
+print("Currect Process ID")
+print(os.getpid())
 print ("-------------------------------\n")
