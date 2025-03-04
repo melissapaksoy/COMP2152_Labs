554ff08b16625bd7c83fd31da2798ac8cd476dbc
Melissa <melisapaksoy@hotmail.com>
2025-03-04 10:33:36 -0500

    adding current processor type

--- Week09/sys_operations.py
+++ Week09/sys_operations.py
@@ -1,4 +1,9 @@
 import platform
 
 print("Current machine type")
-print(platform.machine()) # Apple M1, M2, M3, M4, Raspberry Pi : arm64; for intel/AMD: x86_64
+print(platform.machine()) # Apple M1, M2, M3, M4, Raspberry Pi : arm64; for intel/AMD: x86
+print ("-------------------------------\n")
+
+print("Current Processor Type:")
+print(platform.architecture())
+print ("-------------------------------\n")
